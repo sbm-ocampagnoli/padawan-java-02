434673ed40827514867c1b457c57d1fd3ebfcc33
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-07-02 12:22:28 -0300

    Segunda instância.

--- bytebank/src/bytebank/CriarConta.java
+++ bytebank/src/bytebank/CriarConta.java
@@ -5,5 +5,14 @@ public class CriarConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+
+		System.out.println("primeira conta tem " + primeiraConta.saldo);
+		System.out.println("segunda conta tem " + segundaConta.saldo);
 	}
 }
