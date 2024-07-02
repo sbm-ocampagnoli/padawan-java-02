cba7bfc22c1e5ff36346b1a4c7089476e60ec201
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-07-02 12:26:41 -0300

    Valores default de atributos

--- bytebank/src/bytebank/CriarConta.java
+++ bytebank/src/bytebank/CriarConta.java
@@ -14,5 +14,13 @@ public class CriarConta {
 
 		System.out.println("primeira conta tem " + primeiraConta.saldo);
 		System.out.println("segunda conta tem " + segundaConta.saldo);
+
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+
+		System.out.println(segundaConta.agencia);
+
+		segundaConta.agencia = 146;
+		System.out.println("agora a segunda conta está na agencia " + segundaConta.agencia);
 	}
 }
